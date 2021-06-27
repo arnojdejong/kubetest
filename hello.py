4f32dcfb3564d46d0d0a6b834aeeee67d821999e
Arno <37892588+arnojdejong@users.noreply.github.com>
2021-06-27 09:07:19 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -7,4 +7,4 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 if __name__ == "__main__":
-   app.run(debug=True, use_reloader=False)
+   app.run(host='0.0.0.0', debug=True, use_reloader=False)
